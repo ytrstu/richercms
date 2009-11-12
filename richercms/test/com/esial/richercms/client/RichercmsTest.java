f05b2d212c0146c3f2fbaf36d12c34f924b1d173
richercms <richercms@31c0fa6c-b72d-11de-88a7-9da883da0866>
2009-11-12 14:33:53 +0000

    Added empty test using junitCreator

---
+++ richercms/test/com/esial/richercms/client/RichercmsTest.java
@@ -0,0 +1,24 @@
+package com.esial.richercms.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+/**
+ * GWT JUnit tests must extend GWTTestCase.
+ */
+public class RichercmsTest extends GWTTestCase {
+
+  /**
+   * Must refer to a valid module that sources this class.
+   */
+  public String getModuleName() {
+    return "com.esial.richercms.Richercms";
+  }
+
+  /**
+   * Add as many tests as you like.
+   */
+  public void testSimple() {
+    assertTrue(true);
+  }
+
+}

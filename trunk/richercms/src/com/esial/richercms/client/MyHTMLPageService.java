c7e957f93ea325ff1d674ed360074ea0c6f7ed77
yannick.hertzog@esial.net <yannick.hertzog@esial.net@31c0fa6c-b72d-11de-88a7-9da883da0866>
2010-01-18 16:43:04 +0000

    HTML Persistence - Tests Failed

--- trunk/richercms/src/com/esial/richercms/client/MyHTMLPageService.java
+++ trunk/richercms/src/com/esial/richercms/client/MyHTMLPageService.java
@@ -1,7 +1,9 @@
 package com.esial.richercms.client;
 
 import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
+@RemoteServiceRelativePath("pages")
 public interface MyHTMLPageService extends RemoteService {
 	public void addPage(String content) throws NotLoggedInException;
 	public void deletePage(String qualified_name) throws NotLoggedInException;

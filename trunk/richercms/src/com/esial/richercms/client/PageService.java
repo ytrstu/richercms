a6ffb1229f4366cae43d988443e80825a9a500db
yannick.hertzog@esial.net <yannick.hertzog@esial.net@31c0fa6c-b72d-11de-88a7-9da883da0866>
2010-01-25 16:22:12 +0000

    Improved Page model.

--- trunk/richercms/src/com/esial/richercms/client/PageService.java
+++ trunk/richercms/src/com/esial/richercms/client/PageService.java
@@ -5,5 +5,6 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 @RemoteServiceRelativePath("pages")
 public interface PageService extends RemoteService {
-	public void addPage(String unique_name) throws NotLoggedInException;
+	public void addPage(String browser_title,String page_title, String url_name,String 
+			description, String content) throws NotLoggedInException;
 }

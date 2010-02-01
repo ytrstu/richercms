db3ba20fa675ec99104842d1ef3931a1b3c870e6
stephanie.maindron@gmail.com <stephanie.maindron@gmail.com@31c0fa6c-b72d-11de-88a7-9da883da0866>
2010-02-01 17:32:47 +0000

    Internationalization files modified

--- trunk/richercms/src/com/esial/richercms/client/RicherConstants.java
+++ trunk/richercms/src/com/esial/richercms/client/RicherConstants.java
@@ -7,12 +7,25 @@ public interface RicherConstants extends Constants{
 	@DefaultStringValue("RicherCMS Project")
 	String appTitle();
 	
+	/*Generic parts*/
+	@DefaultStringValue("Cancel")
+	String bpcancel();
+	
 	/*Administration part*/
 	@DefaultStringValue("Administration")
 	String admin();
+	
 	@DefaultStringValue("Add new user")
-	String bpadd();
+	String bpaddu();
+	
 	@DefaultStringValue("Edit an existing user")
-	String bpedit();
+	String bpeditu();
+	
+	@DefaultStringValue("Editor Language :")
+	String lblCmslang();
+	
+	@DefaultStringValue("Website Created default language :")
+	String lblSitelang();
+	
 	
 }

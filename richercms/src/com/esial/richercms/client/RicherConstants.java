529c9ffe5cbe7688d040a29ba006a56b9a2f428d
stephanie.maindron <stephanie.maindron@gmail.com>
2010-03-01 10:14:00 +0000

    Internationalization of CmsPageEdition

--- richercms/src/com/esial/richercms/client/RicherConstants.java
+++ richercms/src/com/esial/richercms/client/RicherConstants.java
@@ -4,6 +4,7 @@ import com.google.gwt.i18n.client.Constants;
 
 public interface RicherConstants extends Constants{
 	
+	
 	@DefaultStringValue("RicherCMS Project")
 	String appTitle();
 	
@@ -26,6 +27,41 @@ public interface RicherConstants extends Constants{
 	
 	@DefaultStringValue("Website Created default language :")
 	String lblSitelang();
+	
+	@DefaultStringValue("Create page")
+	String bpcreatepage();
+	
+	@DefaultStringValue("Edit page")
+	String bpeditpage();
+	
+	@DefaultStringValue("Delete page")
+	String bpdelpage();
+	
+	/*Editor part*/
+	
+	@DefaultStringValue("Browser Title")
+	String browserTitle();
+
+	@DefaultStringValue("Page Title")
+	String pageTitle();
+	
+	@DefaultStringValue("URL name")
+	String urlName();
+
+	@DefaultStringValue("Description")
+	String description();
+
+	@DefaultStringValue("Publish date")
+	String publishDate();
+	
+	@DefaultStringValue("Content")
+	String content();
+	
+	@DefaultStringValue("Upload a picture")
+	String uploadPic();
+	
+	@DefaultStringValue("Picture name")
+	String picName();
 	
 	
 }
--- richercms/src/com/esial/richercms/client/view/AdminView.java
+++ richercms/src/com/esial/richercms/client/view/AdminView.java
@@ -6,7 +6,6 @@ import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.ChangeListener;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;

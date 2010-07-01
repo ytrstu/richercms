368f2033a79a920a919089bce15bde27e77c08b2
guillaume.homberg <guillaume.homberg@31c0fa6c-b72d-11de-88a7-9da883da0866>
2010-07-01 14:55:10 +0000

    informationPanel : 	- "-" et "_" possible dans les URL. 	- Url par défaut dans le main (index). 	- Test sur les champs obligatoires pour toutes les langues.
    DisplayPage :
    	- Le bouton modifier réapparait après un save and Quit.
    	
    TemplateManager :
    	- Interface opérationnel
    	- Sauvegarde de template dans le datastore
    	- Possibilité de choisir les tag lié au template
    	
    Création des objets métiers pour les templates.
    Création du service RPC pour gèrer les templates

---
+++ richercms/src/com/sfeir/richercms/page/client/TemplateServiceAsync.java
@@ -0,0 +1,15 @@
+package com.sfeir.richercms.page.client;
+
+import java.util.List;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.sfeir.richercms.page.shared.BeanTemplate;
+
+public interface TemplateServiceAsync {
+
+	public void addTemplate(BeanTemplate bean, AsyncCallback<Long> callback);
+	public void getTemplate(Long id, AsyncCallback<BeanTemplate> callback);
+	public void getAllTemplate(AsyncCallback<List<BeanTemplate>> callback);
+	public void upDateTagsTemplate(Long templateId, List<Long> tagIds, AsyncCallback<Void> callback);
+	public void deleteTemplate(Long id, AsyncCallback<Void> callback);
+}
--- richercms/src/com/sfeir/richercms/page/client/view/TemplateManager.java
+++ richercms/src/com/sfeir/richercms/page/client/view/TemplateManager.java
@@ -1,9 +1,18 @@
 package com.sfeir.richercms.page.client.view;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.HasChangeHandlers;
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.event.dom.client.HasKeyPressHandlers;
+import com.google.gwt.event.logical.shared.HasValueChangeHandlers;
 import com.google.gwt.layout.client.Layout.Alignment;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.Label;
@@ -14,6 +23,7 @@ import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.user.client.ui.HTMLTable.CellFormatter;
 import com.sfeir.richercms.page.client.interfaces.ITemplateManager;
+import com.sfeir.richercms.page.client.view.custom.PopUpAddTemplate;
 
 public class TemplateManager extends ResizeComposite implements ITemplateManager {
 
@@ -22,13 +32,17 @@ public class TemplateManager extends ResizeComposite implements ITemplateManager
 	private ListBox listTemplate;
 	private Button addTemplate;
 	private FlexTable tagTable;
+	private PopUpAddTemplate popUpAddTemplate;
+	private HashMap<Long,CheckBox> selectedTags; // idTag, associated CheckBox
 	
 	public Widget asWidget() {
 		return this;
 	}
 
 	public void createView() {
+		this.selectedTags = new HashMap<Long,CheckBox>();
 		this.mainContainer = new DockLayoutPanel(Unit.PX);
+		this.popUpAddTemplate = new PopUpAddTemplate();
 		
 		//title
 		Label title = new Label("Gestion des Templates");
@@ -61,6 +75,9 @@ public class TemplateManager extends ResizeComposite implements ITemplateManager
 		Label tagtitle = new Label("tag possible pour ce template");
 		tagtitle.setStyleName("informationTitle");
 		this.tagTable = new FlexTable();
+		this.tagTable.addStyleName("tagTable");
+		this.tagTable.setCellPadding(5);
+		this.tagTable.setBorderWidth(2);
 		this.addTagTableTitle();
 		templateContainer.add(tagtitle);
 		templateContainer.setWidgetTopHeight(tagtitle, 100, Unit.PX, 160, Unit.PX);
@@ -89,5 +106,114 @@ public class TemplateManager extends ResizeComposite implements ITemplateManager
 		this.tagTable.setText(0, 3, "Description");
 		this.tagTable.setText(0, 4, "tag textuel");
 	}
+	
+	public HasValueChangeHandlers<Boolean> addTagLine(String id,String TagName,
+			String shortLib, String description, boolean textualTag ) {
+		
+		int numRow = this.tagTable.getRowCount();
+		CheckBox selectTag = new CheckBox();
+		selectTag.setFormValue(id);
+		this.selectedTags.put(new Long(id), selectTag);
+		
+		this.tagTable.setWidget(numRow, 0, selectTag);
+		this.tagTable.setText(numRow, 1, TagName);
+		this.tagTable.setText(numRow, 2, shortLib);
+		this.tagTable.setText(numRow, 3, description);
+		
+		if(textualTag)
+			this.tagTable.setText(numRow, 4, "yes");
+		else
+			this.tagTable.setText(numRow, 4, "no");
+		
+		return selectTag;
+	}
+	
+	public List<Long> getSelectedTagsId() {
+		ArrayList<Long> lst = new ArrayList<Long>();
+		
+		for(CheckBox cb : this.selectedTags.values())
+			if(cb.getValue())//take just checked tag
+				lst.add(new Long(cb.getFormValue()));
 
+		return lst;
+	}
+	
+	public HasClickHandlers getBtnAddClick() {
+		return this.addTemplate;
+	}
+	
+	public HasClickHandlers getBtnApplyTagClick() {
+		return this.applyTag;
+	}
+	
+	public HasClickHandlers getPopUpBtnOk() {
+		return this.popUpAddTemplate.ok;
+	}
+	
+	public HasClickHandlers getPopUpBtnCancel() {
+		return this.popUpAddTemplate.cancel;
+	}
+	
+	public HasKeyPressHandlers getPopUpKbEvent() {
+		return this.popUpAddTemplate.getKeyboardEvt();
+	}
+
+	public void showPopUpAddTemplate() {
+			
+		// on vide les champs avant de re-afficher
+		this.popUpAddTemplate.clearField();
+		this.popUpAddTemplate.show();
+	}
+
+	public void hidePopUpAddTemplate() {
+		this.popUpAddTemplate.hide();
+	}
+
+	
+	public String getPopUpNewTempName() {
+		return this.popUpAddTemplate.tbName.getText();
+	}
+	
+	public String getPopUpNewTempDesc() {
+		return this.popUpAddTemplate.taDesc.getText();
+	}
+	
+	public void addTemplateInList(String name, String id) {
+		this.listTemplate.addItem(name, id);
+	}
+
+	public void clearTemplateList() {
+		this.listTemplate.clear();
+	}
+	
+	public void clearTagTable(){
+		this.tagTable.removeAllRows();
+		this.addTagTableTitle();
+	}
+	
+	public HasChangeHandlers getTemplateLstSelection() {
+		return this.listTemplate;
+	}
+	
+	public String getSelectedTemplateId() {
+		return this.listTemplate.getValue(
+				this.listTemplate.getSelectedIndex());
+	}
+	
+	public void unCheckAllTags() {
+		for(CheckBox cb : this.selectedTags.values())
+			cb.setValue(false);
+	}
+	
+	public void checktag(Long tagId) {
+		this.selectedTags.get(tagId).setValue(true);
+	}
+	
+	public void enableApplyTagBtn() {
+		this.applyTag.setEnabled(true);
+	}
+	
+	public void disableApplyTagBtn() {
+		this.applyTag.setEnabled(false);
+	}
 }

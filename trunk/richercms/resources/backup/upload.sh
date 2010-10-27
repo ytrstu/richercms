8eefdca95984bd2c3aed5ce15b357ddc4e0a6b7d
fensminger <fensminger@31c0fa6c-b72d-11de-88a7-9da883da0866>
2010-10-27 18:51:46 +0000

    Mise à jour des scripts de sauvegarde et restauration de la base.

--- trunk/richercms/resources/backup/upload.sh
+++ trunk/richercms/resources/backup/upload.sh
@@ -1,19 +1,48 @@
-appcfg.py upload_data --config_file=config.yml --filename=user.xml --kind=User --url=http://fer.appspot.com/remote_api --application=fer --email=fensminger@gmail.com
+#!/bin/sh
 
-appcfg.py upload_data --config_file=config.yml --filename=translationPage.xml --kind=TranslationPage --url=http://fer.appspot.com/remote_api --application=fer --email=fensminger@gmail.com
+url=http://fer.appspot.com/remote_api
+appli=fer
+bandwidth_limit=50000
+email=VotreEmail
+password=VotreMotDePasse
 
-appcfg.py upload_data --config_file=config.yml --filename=memoryFileItem.xml --kind=MemoryFileItem --url=http://fer.appspot.com/remote_api --application=fer --num_threads=1 --batch_size=1 --bandwidth_limit=50000 --email=fensminger@gmail.com
+appcfg.py upload_data --config_file=config.yml --filename=user.xml --kind=User --bandwidth_limit=$bandwidth_limit --url=$url --application=$appli --email=$email --passin <<-EOF
+$password
+EOF
 
-appcfg.py upload_data --config_file=config.yml --filename=tag.xml --kind=Tag --url=http://fer.appspot.com/remote_api --application=fer --bandwidth_limit=50000 --email=fensminger@gmail.com
+appcfg.py upload_data --config_file=config.yml --filename=translationPage.xml --kind=TranslationPage --bandwidth_limit=$bandwidth_limit --url=$url --application=$appli --email=$email --passin <<-EOF
+$password
+EOF
 
-appcfg.py upload_data --config_file=config.yml --filename=rootArbo.xml --kind=RootArbo --url=http://fer.appspot.com/remote_api --application=fer --bandwidth_limit=50000 --email=fensminger@gmail.com
+appcfg.py upload_data --config_file=config.yml --filename=memoryFileItem.xml --kind=MemoryFileItem --num_threads=1 --batch_size=1 --bandwidth_limit=$bandwidth_limit --url=$url --application=$appli --email=$email --passin <<-EOF
+$password
+EOF
 
-appcfg.py upload_data --config_file=config.yml --filename=logInfo.xml --kind=LogInfo --url=http://fer.appspot.com/remote_api --application=fer --bandwidth_limit=50000 --email=fensminger@gmail.com
+appcfg.py upload_data --config_file=config.yml --filename=tag.xml --kind=Tag --bandwidth_limit=$bandwidth_limit --url=$url --application=$appli --email=$email --passin <<-EOF
+$password
+EOF
 
-appcfg.py upload_data --config_file=config.yml --filename=language.xml --kind=Language --url=http://fer.appspot.com/remote_api --application=fer --bandwidth_limit=50000 --email=fensminger@gmail.com
+appcfg.py upload_data --config_file=config.yml --filename=rootArbo.xml --kind=RootArbo --bandwidth_limit=$bandwidth_limit --url=$url --application=$appli --email=$email --passin <<-EOF
+$password
+EOF
 
-appcfg.py upload_data --config_file=config.yml --filename=configuration.xml --kind=Configuration --url=http://fer.appspot.com/remote_api --application=fer --bandwidth_limit=50000 --email=fensminger@gmail.com
+appcfg.py upload_data --config_file=config.yml --filename=logInfo.xml --kind=LogInfo --bandwidth_limit=$bandwidth_limit --url=$url --application=$appli --email=$email --passin <<-EOF
+$password
+EOF
 
-appcfg.py upload_data --config_file=config.yml --filename=template.xml --kind=Template --url=http://fer.appspot.com/remote_api --application=fer --bandwidth_limit=50000 --email=fensminger@gmail.com
+appcfg.py upload_data --config_file=config.yml --filename=language.xml --kind=Language --bandwidth_limit=$bandwidth_limit --url=$url --application=$appli --email=$email --passin <<-EOF
+$password
+EOF
 
-appcfg.py upload_data --config_file=config.yml --filename=arboPage.xml --kind=ArboPage --url=http://fer.appspot.com/remote_api --application=fer --bandwidth_limit=50000 --email=fensminger@gmail.com
+appcfg.py upload_data --config_file=config.yml --filename=configuration.xml --kind=Configuration --bandwidth_limit=$bandwidth_limit --url=$url --application=$appli --email=$email --passin <<-EOF
+$password
+EOF
+
+appcfg.py upload_data --config_file=config.yml --filename=template.xml --kind=Template --bandwidth_limit=$bandwidth_limit --url=$url --application=$appli --email=$email --passin <<-EOF
+$password
+EOF
+
+appcfg.py upload_data --config_file=config.yml --filename=arboPage.xml --kind=ArboPage --bandwidth_limit=$bandwidth_limit --url=$url --application=$appli --email=$email --passin <<-EOF
+$password
+EOF
+

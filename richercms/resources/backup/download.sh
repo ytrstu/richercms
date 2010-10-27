2a544f37f46954a85daa9b6a8a9bab0aefa317ef
fensminger <fensminger@31c0fa6c-b72d-11de-88a7-9da883da0866>
2010-10-27 18:51:46 +0000

    Mise à jour des scripts de sauvegarde et restauration de la base.

--- richercms/resources/backup/download.sh
+++ richercms/resources/backup/download.sh
@@ -1,23 +1,51 @@
+#!/bin/sh
 # Tres bon PDF : http://bulkloadersample.appspot.com/showfile/bulkloader-presentation.pdf
 # Il faut python 2.5 (< 3.0) et le jdk appengine python installe sur son poste.
-# http://bulkloadersample.appspot.com/showfile/bulkloader-presentation.pdf
 
-appcfg.py download_data --config_file=config.yml --filename=user.xml --kind=User --url=http://20.latest.richer-cms.appspot.com/remote_api --application=richer-cms
+url=http://20.latest.richer-cms.appspot.com/remote_api
+appli=richer-cms
+email=VotreEmail
+password=VotreMotDePasse
 
-appcfg.py download_data --config_file=config.yml --filename=translationPage.xml --kind=TranslationPage --url=http://20.latest.richer-cms.appspot.com/remote_api --application=richer-cms
+rm -rf *.xml images
 
-appcfg.py download_data --config_file=config.yml --filename=memoryFileItem.xml --kind=MemoryFileItem --url=http://20.latest.richer-cms.appspot.com/remote_api --application=richer-cms
+appcfg.py download_data --config_file=config.yml --filename=user.xml --kind=User --url=$url --application=$appli --email=$email --passin <<-EOF
+$password
+EOF
 
-appcfg.py download_data --config_file=config.yml --filename=tag.xml --kind=Tag --url=http://20.latest.richer-cms.appspot.com/remote_api --application=richer-cms
+appcfg.py download_data --config_file=config.yml --filename=translationPage.xml --kind=TranslationPage --url=$url --application=$appli --email=$email --passin <<-EOF
+$password
+EOF
 
-appcfg.py download_data --config_file=config.yml --filename=rootArbo.xml --kind=RootArbo --url=http://20.latest.richer-cms.appspot.com/remote_api --application=richer-cms
+appcfg.py download_data --config_file=config.yml --filename=memoryFileItem.xml --kind=MemoryFileItem --url=$url --application=$appli --email=$email --passin <<-EOF
+$password
+EOF
 
-appcfg.py download_data --config_file=config.yml --filename=logInfo.xml --kind=LogInfo --url=http://20.latest.richer-cms.appspot.com/remote_api --application=richer-cms
+appcfg.py download_data --config_file=config.yml --filename=tag.xml --kind=Tag --url=$url --application=$appli --email=$email --passin <<-EOF
+$password
+EOF
 
-appcfg.py download_data --config_file=config.yml --filename=language.xml --kind=Language --url=http://20.latest.richer-cms.appspot.com/remote_api --application=richer-cms
+appcfg.py download_data --config_file=config.yml --filename=rootArbo.xml --kind=RootArbo --url=$url --application=$appli --email=$email --passin <<-EOF
+$password
+EOF
 
-appcfg.py download_data --config_file=config.yml --filename=configuration.xml --kind=Configuration --url=http://20.latest.richer-cms.appspot.com/remote_api --application=richer-cms
+appcfg.py download_data --config_file=config.yml --filename=logInfo.xml --kind=LogInfo --url=$url --application=$appli --email=$email --passin <<-EOF
+$password
+EOF
 
-appcfg.py download_data --config_file=config.yml --filename=template.xml --kind=Template --url=http://20.latest.richer-cms.appspot.com/remote_api --application=richer-cms
+appcfg.py download_data --config_file=config.yml --filename=language.xml --kind=Language --url=$url --application=$appli --email=$email --passin <<-EOF
+$password
+EOF
 
-appcfg.py download_data --config_file=config.yml --filename=arboPage.xml --kind=ArboPage --url=http://20.latest.richer-cms.appspot.com/remote_api --application=richer-cms
+appcfg.py download_data --config_file=config.yml --filename=configuration.xml --kind=Configuration --url=$url --application=$appli --email=$email --passin <<-EOF
+$password
+EOF
+
+appcfg.py download_data --config_file=config.yml --filename=template.xml --kind=Template --url=$url --application=$appli --email=$email --passin <<-EOF
+$password
+EOF
+
+appcfg.py download_data --config_file=config.yml --filename=arboPage.xml --kind=ArboPage --url=$url --application=$appli --email=$email --passin <<-EOF
+$password
+EOF
+
